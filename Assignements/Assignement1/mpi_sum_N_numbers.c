1af55549b835c210668439d9264d8d14a48d8954
Federico Polli <Polli.F@outlook.it>
2019-11-04 17:36:03 +0100

    .

--- Assignements/Assignement1/mpi_sum_N_numbers.c
+++ Assignements/Assignement1/mpi_sum_N_numbers.c
@@ -8,7 +8,7 @@
 int main ( int argc , char *argv[ ] )
 {
 
-  long long int sum=0, local_sum ; 
+  unsigned long long int sum=0, local_sum ; 
    
   double start_time, end_time;   
   int myid , numprocs , proc ;
@@ -17,17 +17,17 @@ int main ( int argc , char *argv[ ] )
   // master process
   int master = 0;
   int tag = 123;
-  long long int input;
+  unsigned long long int input;
 
   FILE* in_file = fopen("input_parallel_sum.txt", "r"); 
          
   if (! in_file )
     {  
-      printf("oops, file can't be read\n"); 
+      printf("Error reading file\n"); 
       exit(-1); 
     } 
 
-  fscanf(in_file, "%lld", &input );
+  fscanf(in_file, "%llu", &input );
   
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
@@ -36,19 +36,19 @@ int main ( int argc , char *argv[ ] )
   unsigned  int* array=(unsigned int*) calloc(input, sizeof(unsigned int));
   if (array == NULL)
     {
-      printf("Errore nell'allocazione della memoria");
+      printf("Error allocating memory");
       MPI_Finalize();
     }
   unsigned int i;
   unsigned int resto = input%numprocs;
   for (i = 0; i < input; i++)
     array[i] = i + 1;
-  unsigned int N =input/numprocs;
-// take time of processors after initial I/O operation
+  unsigned int N = input/numprocs;
+  // take time of processors after initial I/O operation
   start_time = MPI_Wtime();
   local_sum=0;
   for (i=0; i<N ; i++) {
-    local_sum+=array[myid+i*numprocs];
+    local_sum+=array[i+myid*N];
   }
 
   if (myid == master) { //if I am the master process gather results from others
@@ -65,18 +65,14 @@ int main ( int argc , char *argv[ ] )
     printf ( "\n # walltime on master processor : %10.8f \n", end_time - start_time ) ;
 
   }
-  else {   // for all the slave processes send results to the master /
-
-//    printf ( " Processor %d sending results = %llu to master process \n", myid, local_M) ;
-//    int time_to_sleep=1*myid;
-//    sleep(time_to_sleep);
-
-    MPI_Ssend(&local_sum , 1 ,MPI_LONG_LONG, master , tag ,MPI_COMM_WORLD) ;
-    end_time=MPI_Wtime();
-    printf ( "\n # partial sum on processor %i: %lld \n", myid, local_sum);
-    printf ( "\n # walltime on processor %i : %10.8f \n",myid, end_time - start_time ) ;
-  }
+  else
+    {  
+      MPI_Ssend(&local_sum , 1 ,MPI_LONG_LONG, master , tag ,MPI_COMM_WORLD) ;
+      end_time=MPI_Wtime();
+      printf ( "\n # partial sum on processor %i: %llu \n", myid, local_sum);
+      printf ( "\n # walltime on processor %i : %10.8f \n",myid, end_time - start_time ) ;
+    }
   MPI_Finalize() ; // let MPI finish up /
   if (myid==master)
-  printf ( "\n # total sum: %lld \n", sum); 
+    printf ( "\n # total sum: %llu \n", sum); 
 }
--- Assignements/Assignement1/mpi_sum_N_numbers_2.c
+++ Assignements/Assignement1/mpi_sum_N_numbers_2.c
@@ -8,7 +8,7 @@
 int main ( int argc , char *argv[ ] )
 {
 
-  long long int sum=0, local_sum ; 
+  unsigned long long int sum=0, local_sum ; 
    
   double start_time, end_time;   
   int myid , numprocs , proc ;
@@ -17,35 +17,49 @@ int main ( int argc , char *argv[ ] )
   // master process
   int master = 0;
   int tag = 123;
+  unsigned long long int input;
+
+  FILE* in_file = fopen("input_parallel_sum.txt", "r"); 
+         
+  if (! in_file )
+    {  
+      printf("Error reading file\n"); 
+      exit(-1); 
+    } 
+
+  fscanf(in_file, "%llu", &input );
   
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
-  if ( argc <=1) {
-    fprintf (stderr , " Usage : mpi -np n %s number_of_iterations \n", argv[0] ) ;
-    MPI_Finalize() ;
-    exit(-1) ;
-  }
 
-  unsigned long long int* array=(unsigned long long int*) calloc(atoll(argv[1]), sizeof(unsigned long long int));
-  unsigned long long int i;
-  long long int resto = atoll(argv[1])%numprocs;
-  for (i = 0; i < atoll(argv[1]); i++)
+  unsigned int* array=(unsigned  int*) calloc(input, sizeof(unsigned int));
+  unsigned int i;
+  unsigned int resto = input%numprocs;
+  for (i = 0; i < input; i++)
     array[i] = i + 1;
-  long long int N = atoll(argv[1])/numprocs;
+  unsigned int N = input/numprocs;
 // take time of processors after initial I/O operation
   start_time = MPI_Wtime();
   local_sum=0;
   for (i=0; i<N ; i++) {
-    local_sum+=array[myid+i*numprocs];
+    local_sum+=array[myid*N+i];
   }
+  if (myid==0)
+    {
+      printf("\n # partial sum on master processor: %llu\n", local_sum);
+    }
+  else
+    {
+      printf ("\n # partial sum on processor %d: %llu\n", myid, local_sum);
+	}
   MPI_Reduce(&local_sum, &sum, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   end_time=MPI_Wtime(); 
 
   MPI_Finalize() ; // let MPI finish up /
-  for (i=atoll(argv[1])-resto; i < atoll(argv[1]); i++)
+  for (i=input-resto; i < input; i++)
       sum+=array[i];
   if (myid==master)
-  printf ( "\n # total sum: %lld \n", sum); 
+  printf ( "\n # total sum: %llu \n", sum); 
 }
