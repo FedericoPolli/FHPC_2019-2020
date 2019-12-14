4b5e4856bcc6a97cc0d053ee41a4b3426c4a6de1
FedericoPolli <polli.f@outlook.it>
2019-12-14 09:18:04 +0100

    M

--- Assignements/Assignment2/binary_search_openMP.c
+++ Assignements/Assignment2/binary_search_openMP.c
@@ -1,31 +1,3 @@
-
-/* ────────────────────────────────────────────────────────────────────────── *
- │                                                                            │
- │ This file is part of the exercises for the Lectures on                     │
- │   "Foundations of High Performance Computing"                              │
- │ given at                                                                   │
- │   Master in HPC and                                                        │
- │   Master in Data Science and Scientific Computing                          │
- │ @ SISSA, ICTP and University of Trieste                                    │
- │                                                                            │
- │ contact: luca.tornatore@inaf.it                                            │
- │                                                                            │
- │     This is free software; you can redistribute it and/or modify           │
- │     it under the terms of the GNU General Public License as published by   │
- │     the Free Software Foundation; either version 3 of the License, or      │
- │     (at your option) any later version.                                    │
- │     This code is distributed in the hope that it will be useful,           │
- │     but WITHOUT ANY WARRANTY; without even the implied warranty of         │
- │     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          │
- │     GNU General Public License for more details.                           │
- │                                                                            │
- │     You should have received a copy of the GNU General Public License      │
- │     along with this program.  If not, see <http://www.gnu.org/licenses/>   │
- │                                                                            │
- * ────────────────────────────────────────────────────────────────────────── */
-
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -56,10 +28,6 @@ int mybsearch(int *data, int M, int N, int Key)
 
     mid = (low + high) / 2;
   }
-
-  /* if ( Key == data[low] ) */
-  /*   return 0; */
-  /* else */
   return -1;
 }
 
@@ -121,11 +89,11 @@ int main(int argc, char **argv)
       search = malloc(Nsearch * sizeof(int));
       srand(time(NULL));
       data = malloc(tot * sizeof(int));
-      for (i = 0; i < Nsearch; i++)
-	search[i] = rand() % N; 
+      /* for (i = 0; i < Nsearch; i++) */
+      /* 	search[i] = rand() % N;  */
 
-      for (i = 0; i < tot; i++)
-	data[i] = i;
+      /* for (i = 0; i < tot; i++) */
+      /* 	data[i] = i; */
       printf("performing %d lookups on %d data..\n", Nsearch, N);
       printf("number of threads %d \n", nthreads);
     }
@@ -134,13 +102,13 @@ int main(int argc, char **argv)
     
     int my_thread_id = omp_get_thread_num();
     
-/* #pragma omp parallel for */
-/*     for (i = 0; i < Nsearch; i++) */
-/*       search[i] = rand() % N; */
+#pragma omp parallel for
+    for (i = 0; i < Nsearch; i++)
+      search[i] = rand() % N;
 
-/* #pragma omp parallel for */
-/*     for (i = 0; i < tot; i++) */
-/*       data[i] = i; */
+#pragma omp parallel for
+    for (i = 0; i < tot; i++)
+      data[i] = i;
     
 #pragma omp parallel for schedule(dynamic)
     for (i = 0; i < Nsearch; i++)
