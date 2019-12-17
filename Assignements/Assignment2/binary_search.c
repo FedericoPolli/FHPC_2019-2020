11719e9adc5294d97e63329d7fb97201a027c823
FedericoPolli <polli.f@outlook.it>
2019-12-17 14:36:53 +0100

    M

--- Assignements/Assignment2/binary_search.c
+++ Assignements/Assignment2/binary_search.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <omp.h>
 
+//binary search function
 
 int mybsearch(int *data, int M, int N, int Key)
 {
@@ -27,8 +28,10 @@ int mybsearch(int *data, int M, int N, int Key)
   return -1;
 }
 
+
 #define N_DEFAULT  (100000000)
 #define N_search_DEFAULT (N_DEFAULT / 5)
+
 
 int main(int argc, char **argv)
 {
@@ -39,6 +42,7 @@ int main(int argc, char **argv)
   
   int found = 0;
 
+  //check for some inputs, otherwise use default variables
   if(argc > 1)
     N = atoi( *(argv+1) );
   else
@@ -49,6 +53,8 @@ int main(int argc, char **argv)
   else
     Nsearch = N_search_DEFAULT;
 
+  //if openMP is not defined use the serial algorithm
+  
 #if !defined(_OPENMP)
 
   printf("performing %d lookups on %d data..\n", Nsearch, N);
@@ -72,41 +78,54 @@ int main(int argc, char **argv)
   
 #else
 
-#pragma omp parallel
+//pass the pointer as firstprivate to make each thread allocate the memory it needs
+#pragma omp parallel firstprivate(data)  
   {
-#pragma omp master
+
+    //have a single thread allocate and initialise the array of keys
+#pragma omp single
     {
       nthreads = omp_get_num_threads();
+
+      //make sure that the problem size is divisible by the number of threads
       r=N%nthreads;
       tot=N+r;
-      printf("performing %d lookups on %d data..\n", Nsearch, N);
-      printf("number of threads %d \n", nthreads);
+      
       search = malloc(Nsearch * sizeof(int));
       srand(time(NULL));
-      data = malloc(tot * sizeof(int));
       for (i = 0; i < Nsearch; i++)
-  	search[i] = rand() % N;
-
-      for (i = 0; i < tot; i++)
-  	data[i] = i;
+	search[i] = rand() % N;
+      printf("performing %d lookups on %d data..\n", Nsearch, N);
+      printf("number of threads %d \n", nthreads);
     }
+    
     int my_thread_id = omp_get_thread_num();
 
-#pragma omp barrier
-
-      
-#pragma omp parallel for schedule(dynamic)
+    //each thread calculates where to allocate the memory in order to avoid clashes but still have the array contiguous in memory
+    //Then each thread allocates and inizialises its own memory
+    data+=my_thread_id*tot/nthreads;
+    data = malloc((tot/nthreads) * sizeof(int));
     
+#pragma omp parallel for
+    //notice that this way there are some extra elements which are inizialised,
+    //but since the keys are all less than N by construction this does not affect the correctness of the code
+    //If that should change one must modify the code accordingly
+    //for example by making the last thread initialise the extra elements to 0 or -1 or whatever
+    for (i = 0; i < tot/nthreads; i++)
+      data[i] = tot*my_thread_id/nthreads+i;
+
+
+    //each thread searches for the key in its own dataset
+#pragma omp parallel for schedule(dynamic)    
     for (i = 0; i < Nsearch; i++)
-      if( mybsearch(data, my_thread_id*tot/nthreads, (my_thread_id+1)*tot/nthreads-1, search[i]) >= 0)	      
-#pragma omp atomic
+      if( mybsearch(data, 0, tot/nthreads, search[i]) >= 0)	      
+#pragma omp atomic update
 	found++;  
+    free(data);
   }
 #endif
   
-  printf("found: %d\n", found); 
-  free(data);
+  printf("found %d values out of %d in the given dataset \n", found, Nsearch); 
   free(search);
-
   return 0;
 }
--- Assignements/Assignment2/grafici_es_0.m
+++ Assignements/Assignment2/grafici_es_0.m
@@ -1,11 +1,11 @@
-serial_time=12.38;
-parallel_times_touch_first=[9.74, 8.35, 7.92, 7.84, 7.91, 7.94];
-parallel_times_touch_all=[6.31, 3.32, 1.81, 1.33, 1.21, 1.12];
-speedup_touch_first=serial_time./parallel_times_touch_first
+serial_time=8.70;
+%parallel_times_touch_first=[9.74, 8.35, 7.92, 7.84, 7.91, 7.94];
+parallel_times_touch_all=[5.5, 3.7, 2.9, 2.7, 2.55, 2.4];
+%speedup_touch_first=serial_time./parallel_times_touch_first
 speedup_touch_all=serial_time./parallel_times_touch_all
-estimated_serial_part_touch_first=zeros(1, 6);
+%estimated_serial_part_touch_first=zeros(1, 6);
 %estimated_serial_part_touch_all=zeros(1, 6);
-%num_threads=[2, 4, 8, 12, 16, 20];
+num_threads=[2, 4, 8, 12, 16, 20];
 %for i=1:6
   %  estimated_serial_part_touch_first(i)=(1/speedup_touch_first(i)-1/num_threads(i))/(1-1/num_threads(i));
     %estimated_serial_part_touch_all(i)=(1/speedup_touch_all(i)-1/num_threads(i))/(1-1/num_threads(i));
@@ -17,13 +17,13 @@ estimated_serial_part_touch_first=zeros(1, 6);
 
  %scatter(num_threads, speedup_touch_first, 'filled', 'r')
 % hold on
-% scatter(num_threads, speedup_touch_all, 'filled', 'b')
+scatter(num_threads, speedup_touch_all, 'filled', 'b')
 
 
 %estimated_serial_part_touch_first
 %estimated_serial_part_touch_all
- %axis([0 20 0 5])
- %grid on
- %xlabel('number of threads')
- %ylabel('speedup')
- %legend('binary search speedup')
+ axis([0 20 0 5])
+ grid on
+ xlabel('number of threads')
+ ylabel('speedup')
+ legend('binary search speedup')
